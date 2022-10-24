630459a660f1e20de7116919bb6d1c1037017993
saheedibrahim <saolabram@gmail.com>
2022-10-24 04:51:00 +0100

    register the models in admin.py

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,30 @@
+from email.policy import default
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length = 400)
+    last_name = models.CharField(max_length = 400)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name
+
+class Song(models.Model):
+    title = models.ForeignKey(Artiste, on_delete = models.PROTECT)
+    date_released = models.DateField(default = datetime.today)
+    likes = models.IntegerField()
+    artiste_id = models.CharField(max_length = 400)
+
+    def __str__(self):
+        return self.title
+
+class Lyric(models.Model):
+    content = models.ForeignKey(Song, on_delete = models.PROTECT)
+    song_id = models.CharField(max_length = 400)
+
+    def __str__(self):
+        return self.content
+
+
